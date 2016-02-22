4883275ee93e7d9df1feb43574c3df23ec7d819b
clarencenpy <clarencenpy@gmail.com>
2016-02-22 22:18:38 +0800

    add avatar and facebook profile

--- server/social-config.js
+++ server/social-config.js
@@ -7,3 +7,11 @@ ServiceConfiguration.configurations.insert({
     appId: '974399485943358',
     secret: 'b96ac6019827044e4953dcf437f4805f'
 });
+
+Accounts.onCreateUser(function (options, user) {
+    if (options.profile) {
+        options.profile.avatar = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+        user.profile = options.profile;
+    }
+    return user;
+})
