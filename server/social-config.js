cb0ed12d445913e98d1ef4dc40bb7f60e6f388ad
clarencenpy <clarencenpy@gmail.com>
2016-02-22 21:38:44 +0800

    facebook login

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '974399485943358',
+    secret: 'b96ac6019827044e4953dcf437f4805f'
+});
