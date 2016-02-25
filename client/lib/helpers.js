010c92a75f14785c30f59b1041008ec11a79d805
clarencenpy <clarencenpy@gmail.com>
2016-02-26 03:40:16 +0800

    countdown days from a week away

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -5,6 +5,11 @@ Template.registerHelper('getFirstName', function (fullName) {
 
 Template.registerHelper('moCalendarDetailed', function (time) {
     if (time !== undefined) {
+        let now = moment()
+        let diff = moment(time).diff(now, 'days')
+        if (diff <= 7) {
+            return diff + ' Days Left!'
+        }
         return moment(time).calendar(null, {
             sameDay: '[Today]',
             nextDay: '[Tomorrow]',
