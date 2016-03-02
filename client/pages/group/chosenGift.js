6cb40f665e016f2b5b787f21bd1b058422f8d3fa
clarencenpy <clarencenpy@gmail.com>
2016-03-03 03:36:33 +0800

    fix bug: still have winning gift even if no votes were cast - need to think of better logic for deciding which gift wins

--- client/pages/group/chosenGift.js
+++ client/pages/group/chosenGift.js
@@ -11,7 +11,7 @@ Template.chosenGift.helpers({
         let groupId = FlowRouter.getParam('id')
         let ideas = Groups.findOne(groupId).giftIdeas
         //find the idea with the most votes
-        let winner
+        let winner = ideas[0]
         let maxVotes = 0
         _.each(ideas, function (idea) {
             if (idea.votes.length > maxVotes) {
