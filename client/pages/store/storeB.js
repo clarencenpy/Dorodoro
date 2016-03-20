6b03f3de29c8faa5bfa572cc7f53e4cb901f5cf1
clarencenpy <clarencenpy@gmail.com>
2016-03-20 18:28:43 +0800

    add dialog box

---
+++ client/pages/store/storeB.js
@@ -0,0 +1,111 @@
+Template.storeB.onCreated(function () {
+    const template = this
+
+    //gift selection mode
+    //{
+    //    group:
+    //    selection: [products]
+    //}
+
+
+    Session.set('searchQuery', {
+        title: '',
+        to: 5000,
+        from: 0,
+        categories: [],
+        sort: {
+            sortby: 'likes',
+            by: -1
+        }
+    })
+})
+
+Template.storeB.onRendered(function () {
+
+    const template = this
+
+    Session.set('header', 'Gift Store')
+    this.$('.rateit').rateit();
+
+    //add listener to search input
+    let $search = template.$('#search-input')
+    $search.on('keyup', function (event) {
+        let query = $search.val()
+    })
+
+})
+
+Template.storeB.helpers({
+    products() {
+        let queryParams = {}
+        let sq = Session.get('searchQuery')
+        let sortBy = sq.sort.sortby
+
+        let products = Products.find({isUserSubmitted: {$in: [null, undefined, false]}}).fetch()
+        products = _.map(products, function (product) {
+            if (sq.title.length > 0 && product.title.toUpperCase().search(sq.title.toUpperCase()) < 0) product.hide = true
+            if (product.price < sq.from || product.price > sq.to) product.hide = true
+            let inAllCategories = true
+            _.each(sq.categories, function (cat) {
+                if (_.indexOf(product.category, cat) === -1) inAllCategories = false
+            })
+            if (!inAllCategories) product.hide = true
+            return product
+        })
+
+        if (sortBy === 'likes') {
+            products = _.sortBy(products, 'likes')
+            if (sq.sort.by < 0) products.reverse()
+        }
+        if (sortBy === 'price') {
+            products = _.sortBy(products, 'price')
+            if (sq.sort.by < 0) products.reverse()
+        }
+        /*if(sortby === 'rating'){
+         return Products.find(queryParams, {sort: { $avg{}: sq.sort.by }})
+         }*/
+        return products
+    },
+
+    groups() {
+        return Groups.find({$or: [{members: Meteor.userId()}, {createdBy: Meteor.userId()}]}, {sort: {date: -1}})
+    },
+
+    productRating(arr){
+        return _.reduce(arr, function(memo, num) {
+                return memo + num;
+            }, 0) / (arr.length === 0 ? 1 : arr.length);
+    },
+
+    selectedProduct() {
+        return Session.get('selectedProduct')
+    },
+    showFilter(){
+        return Session.get('showFilter')
+    }
+})
+
+Template.storeB.events({
+    'click .group-block'() {
+        Session.set('selectedProduct', this)
+    },
+    'click .search-filter'() {
+        Session.set('showFilter', true)
+    },
+    'keyup #search-input'(event, template) {
+        let title = $(event.target).val()
+        let sq = Session.get('searchQuery') || {}
+        sq.title = title
+        Session.set('searchQuery', sq)
+    },
+    
+    'click .dialog .btn-close'(event) {
+        $(event.target).closest('.dialog').removeClass("dialog--open").addClass("dialog--close")
+    }
+
+})
+
+Template.storeB.onDestroyed(function () {
+    Session.set('header', null)
+})
+
