63f2d068439c6078900b5e17854791b5b879b32e
clarencenpy <clarencenpy@gmail.com>
2016-03-20 18:39:38 +0800

    cleanup unused files

--- client/pages/calendar.js
+++
@@ -1,6 +0,0 @@
-Template.calendar.onRendered(function () {
-    Session.set('header', 'Calendar')
-})
-Template.calendar.onDestroyed(function () {
-    Session.set('header', null)
-})
--- client/pages/giftSuggestions.js
+++
@@ -1,7 +0,0 @@
-Template.giftSuggestions.onRendered(function () {
-    const instance = this;
-    const sortable = new Sortable(instance.$('#giftsContainer')[0], {
-        animation: 0,
-        ghostClass: "sortable-ghost"  // Class name for the drop placeholder
-    })
-});
